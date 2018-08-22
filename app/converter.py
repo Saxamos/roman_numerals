25fa9b5b8e88b1a600493c8ff6511e78fceb7c73
Samuel <samuel.rochette@yahoo.fr>
2018-08-22 11:54:16 +0200

    refactor

--- app/converter.py
+++ app/converter.py
@@ -4,25 +4,23 @@ ROMAN_TO_NUMERAL = {1000: 'M', 500: 'D', 100: 'C', 50: 'L', 10: 'X', 5: 'V', 1: 
 
 def convert(number):
     assert isinstance(number, int) and 0 < number < 3000
-
-    roman, remainder = '', number
-    for power_of_10 in DECREASING_POWER_OF_10:
-        remainder, roman = convert_power_of_ten_to_roman(remainder, roman, power_of_10)
-
-    return roman
+    return ''.join([convert_power_of_ten_to_roman(number, power_of_10) for power_of_10 in DECREASING_POWER_OF_10])
 
 
-def convert_power_of_ten_to_roman(number, roman, i):
-    remainder = number % i
-    if remainder != number:
-        if int(number / i) < 4:
-            roman += ROMAN_TO_NUMERAL[i] * int(number / i)
-        elif int(number / i) == 4:
-            roman += ROMAN_TO_NUMERAL[i] + ROMAN_TO_NUMERAL[i * 5]
-        elif int(number / i) == 5:
-            roman += ROMAN_TO_NUMERAL[i * 5]
-        elif 5 < int(number / i) < 9:
-            roman += ROMAN_TO_NUMERAL[i * 5] + ROMAN_TO_NUMERAL[i] * (int(number / i) - 5)
-        else:
-            roman += ROMAN_TO_NUMERAL[i] + ROMAN_TO_NUMERAL[i * 10]
-    return remainder, roman
+def convert_power_of_ten_to_roman(number, power_of_10):
+    number %= (power_of_10 * 10)
+    quotient = int(number / power_of_10)
+
+    if quotient <= 3:
+        return ROMAN_TO_NUMERAL[power_of_10] * quotient
+
+    elif quotient == 4:
+        return ROMAN_TO_NUMERAL[power_of_10] + ROMAN_TO_NUMERAL[power_of_10 * 5]
+
+    elif quotient == 5:
+        return ROMAN_TO_NUMERAL[power_of_10 * 5]
+
+    elif 6 <= quotient <= 8:
+        return ROMAN_TO_NUMERAL[power_of_10 * 5] + ROMAN_TO_NUMERAL[power_of_10] * (quotient - 5)
+
+    return ROMAN_TO_NUMERAL[power_of_10] + ROMAN_TO_NUMERAL[power_of_10 * 10]
