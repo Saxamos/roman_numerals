6aa2386190e4e6b7e5f79ad665957656e804f561
Samuel <samuel.rochette@yahoo.fr>
2018-09-05 18:44:47 +0200

    clean project root

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='roman_numerals',
+    version='1.0',
+    description='A numeral to roman converter',
+    author_email='samuel.rochette@yahoo.com',
+    packages=['roman_numerals'],
+    install_requires=['pytest'],
+)
